059a609392a8ed5dd49a4ad93e0d793d2c8d85d4
wei-py <rabbwei@outlook.com>
2024-05-31 14:25:19 +0800

    auto-import

--- src/page/dataRank/dataRank.vue
+++ src/page/dataRank/dataRank.vue
@@ -63,9 +63,13 @@ async function getPreviewData(startDate, endDate) {
     } else {
       const value = unitConver(data, 2);
       result.value = value.result;
+      result.realValue = value.realValue;
+      result.unit = value.unit;
     }
     return result;
   });
+  const sortable = ["已并网", "签约量", "扫码量", "建设中", "待建设"];
+  previewData.value = lo.sortBy(previewData.value, (n) => sortable.findIndex((i) => i == n.title));
 }
 
 /**
@@ -90,14 +94,12 @@ function onPreviewConfirm(val) {
     const time = val.map((v) => dayjs(v).format(format));
     previewCalen.value = time.join(" 至 ");
     previewCalenShow.value = false;
-    console.log(time);
-    // getpreviewData(...time);
+    getPreviewData(...time);
   } else {
     const select = val.selectedOptions[0];
     previewDate.value = select.value;
     previewDateShow.value = false;
-    console.log(select.time);
-    // getpreviewData(...select.time);
+    getPreviewData(...select.time);
   }
 }
 
@@ -109,7 +111,7 @@ onMounted(() => {
 
 <template>
   <van-cell-group inset class="!mt-2">
-    <van-cell valueClass="flex justify-end" titleClass="h-9 yCenter !flex-none" class="!px-2">
+    <van-cell valueClass="flex justify-end" titleClass="h-9 yCenter !flex-none" class="!px-[20px]">
       <template #title>
         <van-icon name="chart-trending-o" class="bg-[#ffab30] text-white rounded-full p-[3px] !text-[18px] mr-1" />
         <p class="font-bold text-[16px]">数据总览</p>
@@ -121,7 +123,7 @@ onMounted(() => {
           readonly
           @click="previewDateShow = true"
           :border="false"
-          class="!w-[auto] max-w-[30%]"
+          class="!w-[auto] max-w-[25%]"
           input-align="right"
         />
         <van-popup v-model:show="previewDateShow" position="bottom">
@@ -141,27 +143,27 @@ onMounted(() => {
         <van-calendar v-model:show="previewCalenShow" type="range" @confirm="onPreviewConfirm" :min-date="new Date(2010, 1, 1)" />
       </template>
     </van-cell>
-    <div class="flex bg-[#ffddb3] m-3 rounded justify-between px-8 text-white h-8 yCenter">
-      <div>已并网</div>
-      <div>
-        {{ previewData.find((n) => n.title == "已并网")?.value }}
+    <div class="grid grid-cols-2 yCenter bg-[#fef2d9] mx-[20px] rounded gap-x-[20px] justify-between px-[30px] h-[40px]">
+      <div class="text-[#868e96]">已并网</div>
+      <div class="text-[20px] text-[#232323] flex">
+        <div class="mr-[10px]">{{ previewData[0]?.realValue }}</div>
+        <div>{{ previewData[0]?.unit }}</div>
       </div>
     </div>
-    <div class="grid grid-cols-2 px-8">
-      <div v-for="p of previewData.slice(0)" :key="p.title">
-        <div>
+    <div class="grid grid-cols-2 px-[50px] gap-x-[20px] gap-y-[10px] my-[10px]">
+      <div v-for="p of previewData.slice(1)" :key="p.title">
+        <div class="text-[14px] text-[#868e96]">
           {{ p.title }}
         </div>
-        <div>
-          {{ p.value }}
+        <div class="text-[20px] flex">
+          <div class="mr-[10px]">{{ p.realValue }}</div>
+          <div>{{ p.unit }}</div>
         </div>
       </div>
-     
-
     </div>
   </van-cell-group>
   <van-cell-group inset class="!mt-2">
-    <van-cell valueClass="flex justify-end" titleClass="h-9 yCenter !flex-none" class="!px-2">
+    <van-cell valueClass="flex justify-end" titleClass="h-9 yCenter !flex-none" class="!px-[20px]">
       <template #title>
         <van-icon name="chart-trending-o" class="bg-[#ffab30] text-white rounded-full p-[3px] !text-[18px] mr-1" />
         <p class="font-bold text-[16px]">组织排名</p>
@@ -173,7 +175,7 @@ onMounted(() => {
           readonly
           @click="tableDateShow = true"
           :border="false"
-          class="!w-[auto] max-w-[30%]"
+          class="!w-[auto] max-w-[25%]"
           input-align="right"
         />
         <van-popup v-model:show="tableDateShow" position="bottom">
@@ -193,8 +195,8 @@ onMounted(() => {
         <van-calendar v-model:show="tableCalenShow" type="range" @confirm="onTableConfirm" :min-date="new Date(2010, 1, 1)" />
       </template>
     </van-cell>
-    <vxe-table :data="tableData" align="center" header-align="center" max-height="400" stripe>
-      <vxe-column type="seq" width="50" title="序号"></vxe-column>
+    <vxe-table :data="tableData" align="center" header-align="center" max-height="520" stripe>
+      <vxe-column type="seq" width="70" title="序号"></vxe-column>
       <vxe-column title="组织名称" width="200">
         <template #default="{ row }">
           <van-noticeBar :text="row.companyName" color="#1f1f1f" speed="15" />
